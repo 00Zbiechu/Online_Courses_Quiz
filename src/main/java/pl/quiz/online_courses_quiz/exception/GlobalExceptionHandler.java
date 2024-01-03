9b29f747e23350bc5bfebb4d86b9d32e0f178f14
mzbiewski <00zbiewski@gmail.com>
2024-01-04 00:22:35 +0100

    Obsługa wyjątku MissingServletRequestParameterException

--- src/main/java/pl/quiz/online_courses_quiz/exception/GlobalExceptionHandler.java
+++ src/main/java/pl/quiz/online_courses_quiz/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package pl.quiz.online_courses_quiz.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -12,6 +14,14 @@ public class GlobalExceptionHandler {
         model.addAttribute("errorStatus", ex.getHttpStatus());
         model.addAttribute("field", ex.getField());
         model.addAttribute("errorCode", ex.getErrorCode());
+        return "error.html";
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public String handleMissingServletRequestParameterException(MissingServletRequestParameterException ex, Model model) {
+        model.addAttribute("errorStatus", HttpStatus.BAD_REQUEST);
+        model.addAttribute("field", ex.getParameterName());
+        model.addAttribute("errorCode", ex.getMessage());
         return "error.html";
     }
 }
